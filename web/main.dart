07f5fcffd257ed65232966c2fc0e6878f235ec2e
David Notik <dave@woven.co>
2015-12-03 14:58:58 -0500

    Hide header on scroll

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,30 @@
 
 import 'dart:html';
 
+final header = querySelector('.header');
+
 void main() {
   querySelector('.main').text = 'Your Dart app is running.';
+
+  var oldY = 0;
+
+  document.onScroll.listen((_) {
+    var newY = document.body.scrollTop;
+    if ((oldY - newY).abs() > 10) {
+      if (oldY < newY) {
+        hide();
+      } else {
+        show();
+      }
+    }
+    oldY = newY;
+  });
+}
+
+hide() {
+  header.classes.add('hide');
+}
+
+show() {
+  header.classes.remove('hide');
 }
